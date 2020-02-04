8d504dfbe31c5424edb16cc31e4cbcfed183d03b
dienigma <joshi.chinmay09@gmail.com>
2020-02-04 15:05:56 +0530

    Fiished up the Script Part of Rock Paper scissors

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('Hello');
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+  const p = playerSelection.toLowerCase();
+  const c = computerSelection.toLowerCase();
+  if ((p === 'rock' && c === 'paper') || (p === 'rock' && c === 'scissor'))
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  if (p === c) return `The game has tied! Play again!`;
+  if (p === 'paper' && c === 'rock')
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  if (p === 'scissors' && c === 'paper')
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  else return `You Lose! ${computerSelection} beats ${playerSelection}`;
+}
+
+function computerPlay() {
+  const possibilities = ['Rock', 'Paper', 'Scissors'];
+  return possibilities[Math.floor(Math.random() * possibilities.length)];
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const result = prompt(
+      `Welcome to Rock Paper Scissor! Choose one of Rock, Paper or Scissor`
+    );
+    const computerSelection = computerPlay();
+    console.log(playRound(result, computerSelection));
+  }
+}
+
+game();
